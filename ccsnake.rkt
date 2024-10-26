cf3db0cc5e80e28476496e1a12cb6d502af69c59
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-10-26 14:46:18 +0300

    Draw the border of the grid

--- ccsnake.rkt
+++ ccsnake.rkt
@@ -2,20 +2,31 @@
 
 (require 2htdp/image)
 (require 2htdp/universe)
+(require lang/posn)
 
 
 (define SCENE_WIDTH 700)
-(define SCENE_HEIGHT 700)
+(define SCENE_HEIGHT 745)
 (define SCORE_STRING "Score: ")
 (define SCORE_SIZE 20)
-(define SCORE_COLOR "green")
+(define SCORE_COLOR "brown")
 (define SCORE_X 15)
 (define SCORE_Y 15)
+(define GRID_X 0)
+(define GRID_Y 45)
+(define GRID_WIDTH 700)
+(define GRID_HEIGHT 700)
+(define GRID_BORDER_COLOR "black")
 (define BACKGROUND (empty-scene SCENE_WIDTH SCENE_HEIGHT))
 
 (provide (all-defined-out))
 
-(define (render score) (place-image/align (text SCORE_STRING SCORE_SIZE SCORE_COLOR) SCORE_X SCORE_Y "left" "top" BACKGROUND))
+(define (render score) (place-images/align (list (text (string-append SCORE_STRING (number->string score))
+                                                       SCORE_SIZE SCORE_COLOR)
+                                                 (rectangle GRID_WIDTH GRID_HEIGHT "outline" GRID_BORDER_COLOR))
+                                           (list (make-posn SCORE_X SCORE_Y)
+                                                 (make-posn GRID_X GRID_Y))
+                                           "left" "top" BACKGROUND))
 
 (define (main score)
   (big-bang score
--- ccsnake_test.rkt
+++ ccsnake_test.rkt
@@ -2,6 +2,12 @@
 
 (require rackunit)
 (require 2htdp/image)
+(require lang/posn)
 (require "ccsnake.rkt")
 
-(check-equal? (render 0) (place-image/align (text SCORE_STRING SCORE_SIZE SCORE_COLOR) SCORE_X SCORE_Y "left" "top" BACKGROUND))
+(check-equal? (render 0) (place-images/align (list (text (string-append SCORE_STRING (number->string 0))
+                                                         SCORE_SIZE SCORE_COLOR)
+                                                   (rectangle GRID_WIDTH GRID_HEIGHT "outline" GRID_BORDER_COLOR))
+                                             (list (make-posn SCORE_X SCORE_Y)
+                                                   (make-posn GRID_X GRID_Y))
+                                             "left" "top" BACKGROUND))
