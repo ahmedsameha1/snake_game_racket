16a6ca3f84c2d6e228948d83e733f81453d61818
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2024-11-27 10:40:22 +0200

    Add head of the snake and complete the render of the snake body

--- ccsnake.rkt
+++ ccsnake.rkt
@@ -23,15 +23,16 @@
 
 (provide (all-defined-out))
 
-(define-struct game (snake-body score))
+(define-struct game (snake-body head score))
 ;; Game is (make-game  (listof cells) score)
 ;; interp. the current state of a snake game
-;;         with the current cells of the snake body and score
+;;         with the current cells of the snake body, the position of the head of the snake
+;;         and score
 
 (define (render g) (place-images/align (list (text (string-append SCORE_STRING (number->string (game-score g)))
                                                    SCORE_SIZE SCORE_COLOR)
                                              (rectangle GRID_WIDTH GRID_HEIGHT "outline" GRID_BORDER_COLOR)
-                                             (draw-snake-body (game-snake-body g) (empty-scene GRID_WIDTH GRID_WIDTH)))      
+                                             (draw-snake-body (game-snake-body g) (game-head g) (empty-scene GRID_WIDTH GRID_WIDTH)))      
                                        (list (make-posn SCORE_X SCORE_Y)
                                              (make-posn GRID_X GRID_Y)
                                              (make-posn GRID_X GRID_Y))
@@ -39,18 +40,30 @@
 
 ;; List of Numbers, Image -> Image
 ;; Draws the body of the snake over the image of background
-(define (draw-snake-body body bg)
-  (cond [(empty? body) bg]
-        [(= 1 (length body)) 
-         (place-image/align (square CELL_WIDTH "solid" "seagreen")
-                            (* (remainder (car body) COUNT_CELLS) CELL_WIDTH)
-                            (* (quotient (car body) COUNT_CELLS) CELL_WIDTH)
-                            "left" "top" (draw-snake-body (cdr body) bg))] 
-        [else 
-         (place-image/align (square CELL_WIDTH "solid" "green")
-                            (* (remainder (car body) COUNT_CELLS) CELL_WIDTH)
-                            (* (quotient (car body) COUNT_CELLS) CELL_WIDTH)
-                            "left" "top" (draw-snake-body (cdr body) bg))]))
+(define (draw-snake-body body0 head bg)
+  (local [(define (aux body head bg acc)
+            (cond [(empty? body) bg]
+                  [else
+                   (cond [(= head 0) (if (= acc 1) 
+                                         (place-image/align (square CELL_WIDTH "solid" "seagreen")
+                                                            (* (remainder (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            (* (quotient (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            "left" "top" (aux (cdr body) head bg (add1 acc)))
+                                         (place-image/align (square CELL_WIDTH "solid" "green")
+                                                            (* (remainder (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            (* (quotient (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            "left" "top" (aux (cdr body) head bg (add1 acc))))]
+                         [(= head 1) (if (= acc (length body0))
+                                         (place-image/align (square CELL_WIDTH "solid" "seagreen")
+                                                            (* (remainder (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            (* (quotient (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            "left" "top" (aux (cdr body) head bg (add1 acc)))
+                                         (place-image/align (square CELL_WIDTH "solid" "green")
+                                                            (* (remainder (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            (* (quotient (car body) COUNT_CELLS) CELL_WIDTH)
+                                                            "left" "top" (aux (cdr body) head bg (add1 acc))))])]))]
+    (aux body0 head bg 1))
+  )
 
 (define (main score)
   (big-bang score
